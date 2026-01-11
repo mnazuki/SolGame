9dd0665a0daec4b0585dfbff394ef2307006f02d
mnazuki <115564021+mnazuki@users.noreply.github.com>
2026-01-11 18:22:07 +0800

    primitive animations (no tweens)

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -11,7 +11,7 @@ public class PlayerMovement : MonoBehaviour
     public float PlayerJump = 10f;
     [SerializeField]private float currentSpeed;
     public Transform cameraTransform;
-
+    public Animator animator;
     private Rigidbody rb;
 
 
@@ -39,6 +39,22 @@ public class PlayerMovement : MonoBehaviour
             case PlayerState.Idle:
                 if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
                 {
+                    if (Input.GetAxisRaw("Horizontal") > 0)
+                    {
+                        animator.Play("OneHand_Up_Run_F_R90_A");
+                    }
+                    else if(Input.GetAxisRaw("Horizontal") < 0)
+                    {
+                        animator.Play("OneHand_Up_Run_F_L90_A");
+                    }
+                    else if (Input.GetAxisRaw("Vertical") < 0)
+                    {
+                        animator.Play("OneHand_Up_Run_B");
+                    }
+                    else
+                    {
+                        animator.Play("OneHand_Up_Run_F");
+                    }
                     state = PlayerState.Walking;
                 }
                 break;
@@ -46,11 +62,13 @@ public class PlayerMovement : MonoBehaviour
                 if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
                 {
                     state = PlayerState.Idle;
+                    animator.Play("OneHand_Up_Stand_Idle_A_2");
                     currentSpeed = PlayerSpeed;
                 }
                 else if (Input.GetKey(KeyCode.LeftShift))
                 {
                     state = PlayerState.Running;
+                    animator.Play("OneHand_Up_Sprint_F");
                     currentSpeed = PlayerSpeed * PlayerRunning;
                 }
                 else
@@ -62,11 +80,13 @@ public class PlayerMovement : MonoBehaviour
                 if (Input.GetAxisRaw("Horizontal") == 0 && Input.GetAxisRaw("Vertical") == 0)
                 {
                     state = PlayerState.Idle;
+                    animator.Play("OneHand_Up_Stand_Idle_A_2");
                     currentSpeed = PlayerSpeed;
                 }
                 else if (!Input.GetKey(KeyCode.LeftShift))
                 {
                     state = PlayerState.Walking;
+                    animator.Play("OneHand_Up_Run_F");
                     currentSpeed = PlayerSpeed;
                 }
                 break;
